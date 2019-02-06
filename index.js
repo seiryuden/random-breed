a19f375944ea6207e009b1e7e50106af0db0f40d
seiryuden <seiryuden@outlook.com>
2019-02-06 21:28:58 +0900

    v1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let counter = {count : 0};
+
 function getImages(inputText){
     console.log("getImages ran");
     fetch(`https://dog.ceo/api/breed/${inputText}/images/random`)
@@ -18,23 +20,39 @@ function displayImages(responseJson){
         $(".displayed-content").replaceWith(`<img class="displayed-content" src="${responseJson.message}">`);
         console.log(responseJson.message);
         $(".images-section").removeClass("js-hidden");
+        counter.count = 0;
+
+    } else if (responseJson.status == "error" && counter.count == 0){
+        console.log("dasher ran");
+        let splitInput = document.getElementById("input-text").value.split(" ");
+        let dashedInput= splitInput.join("-");
+        getImages(dashedInput);
+        console.log(dashedInput);
+        counter.count++;
+        console.log(counter);
+
+    } else if (responseJson.status == "error" && counter.count == 1){
+        console.log("reverser ran");
+        let splitInput = document.getElementById("input-text").value.split(" ");
+        let reorderedInput= splitInput.reverse().join("-");
+        getImages(reorderedInput);
+        console.log(reorderedInput);
+        counter.count++;
 
     } else {
-
+        console.log("error messager ran");
         $(".displayed-content").replaceWith(`<h3 class="displayed-content">${responseJson.message}</h3>`);
         $(".images-section").removeClass("js-hidden");
-    }
-
-    
-
-        
+        counter.count = 0;
+    }     
 }
 
 function watchForm(){
     console.log("watchForm ran");
     $("form").submit(event => {
         event.preventDefault();
-        let inputText = document.getElementById("input-text").value;
+        let rawInput = document.getElementById("input-text").value;
+        let inputText= rawInput.replace(/\s/g, "");
         console.log(inputText);
         getImages(inputText);
     })
