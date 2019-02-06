07ebe035d314e5cfa8d13fccf0589e9df9c3f254
seiryuden <seiryuden@outlook.com>
2019-02-06 22:51:30 +0900

    v2

--- index.js
+++ index.js
@@ -4,48 +4,56 @@ let counter = {count : 0};
 
 function getImages(inputText){
     console.log("getImages ran");
+    console.log(`searching for ${inputText}`);
     fetch(`https://dog.ceo/api/breed/${inputText}/images/random`)
     .then(response => response.json())
-    .then(responseJson => displayImages(responseJson))
-    .catch(error => alert ("Sorry, something went wrong!. Try again later."));
+    .then(responseJson => responseHandler(responseJson))
+    .catch(error => alert ("Sorry, something went wrong!. Check your internet connection or try again later."));
     
 }
 
-function displayImages(responseJson){
+function responseHandler(responseJson){
     console.log(responseJson);
-    console.log("displayImages ran");
+    console.log("responseHandler ran");
     
     if (responseJson.status == "success"){
 
-        $(".displayed-content").replaceWith(`<img class="displayed-content" src="${responseJson.message}">`);
-        console.log(responseJson.message);
-        $(".images-section").removeClass("js-hidden");
+        displayImages(responseJson);
         counter.count = 0;
 
     } else if (responseJson.status == "error" && counter.count == 0){
-        console.log("trying inserting dash in the middle");
+        console.log("No matches. Trying inserting dash in the middle of input text");
         let splitInput = document.getElementById("input-text").value.split(" ");
         let dashedInput= splitInput.join("-");
-        console.log(`searching for ${dashedInput}`);
         getImages(dashedInput);
         
         counter.count++;
         console.log(counter);
 
     } else if (responseJson.status == "error" && counter.count == 1){
-        console.log("trying reversing word order");
+        console.log("No matches. Trying reversing word order of input text");
         let splitInput = document.getElementById("input-text").value.split(" ");
         let reorderedInput= splitInput.reverse().join("-");
-        console.log(`searching for ${reorderedInput}`);
         getImages(reorderedInput);
         counter.count++;
 
     } else {
-        console.log("breed not found message");
-        $(".displayed-content").replaceWith(`<h3 class="displayed-content">${responseJson.message}</h3>`);
-        $(".images-section").removeClass("js-hidden");
+        displayNotFound(responseJson);
         counter.count = 0;
     }     
+}
+
+function displayImages(succesfulResponse){
+    $(".displayed-content").replaceWith(`<img class="displayed-content" src="${succesfulResponse.message}">`);
+        console.log(succesfulResponse.message);
+        $(".images-section").removeClass("js-hidden");
+}
+
+function displayNotFound(responseJson){
+    console.log("Breed not found message");
+    $(".displayed-content").replaceWith(`<h3 class="displayed-content">${responseJson.message}</h3>`);
+    $(".images-section").removeClass("js-hidden");
+    
 }
 
 function watchForm(){
@@ -54,7 +62,7 @@ function watchForm(){
         event.preventDefault();
         let rawInput = document.getElementById("input-text").value;
         let inputText= rawInput.replace(/\s/g, "");
-        console.log(inputText);
+        console.log(`input: ${inputText}`);
         getImages(inputText);
     })
 }
